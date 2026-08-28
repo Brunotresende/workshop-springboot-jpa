a67b65944fcf209bb6c0a02c34b41cb7b432e76f
Brunotresende <brunotresende2020@gmail.com>
2026-08-28 13:50:33 -0300

    Service layer, component registration

---
+++ src/main/java/com/projetomaxvendas/maxvendas/services/UserService.java
@@ -0,0 +1,28 @@
+package com.projetomaxvendas.maxvendas.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projetomaxvendas.maxvendas.entities.User;
+import com.projetomaxvendas.maxvendas.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+		
+	}
+	
+	public User findById(Long Id) {
+		Optional<User> obj = repository.findById(Id);
+		return obj.get();
+	}
+
+}
