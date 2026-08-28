d974528e802a42b06842a9da538d8f839d4fb81a
Brunotresende <brunotresende2020@gmail.com>
2026-08-28 10:40:04 -0300

    projeto criado

---
+++ src/main/java/com/projetomaxvendas/maxvendas/entities/User.java
@@ -0,0 +1,5 @@
+package com.projetomaxvendas.maxvendas.entities;
+
+public class User {
+
+}
---
+++ src/test/java/com/projetomaxvendas/maxvendas/MaxvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetomaxvendas.maxvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaxvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
